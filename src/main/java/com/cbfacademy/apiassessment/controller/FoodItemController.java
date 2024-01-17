7475947a428c67f2d1367e1aeedb03f9691f528b
Mariejlo <mjlricorico@gmail.com>
2024-01-17 23:58:43 +0100

    Checked misspelled words on model folder

--- src/main/java/com/cbfacademy/apiassessment/controller/FoodItemController.java
+++ src/main/java/com/cbfacademy/apiassessment/controller/FoodItemController.java
@@ -6,4 +6,4 @@ public class FoodItemController {
 //Handles all customer interactions 
 //Needs Attributes, Methods, and Constructor (check PascalCase name convenction)
 //Optional classes NutritionInfo.java and FoodCategory.java
-//Remember each class single responsability!
+//Remember each class single responsibility!
