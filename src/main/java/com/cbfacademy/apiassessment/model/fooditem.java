20f140268c80f77453d6b113084a0d5d579c07ce
Mariejlo <mjlricorico@gmail.com>
2024-01-18 01:46:38 +0100

    Completed FoodItem with attributes, methods and annotations. Code cleaner and in line with conventions Some visual errors still prevalent

--- src/main/java/com/cbfacademy/apiassessment/model/fooditem.java
+++ src/main/java/com/cbfacademy/apiassessment/model/fooditem.java
@@ -1,32 +1,50 @@
-package com.cbfacademy.apiassessment.model;
-
-import org.springframework.stereotype.Indexed;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-@Entity 
-public class foodItem {
-    public static void main(String[] args) {
-       @Id 
-       @GeneratedValue(strategy = GenerationType.AUTO)
-       Long id; //Unique identifier for each food item (check if private is needed?) 
-       String name; //Name of the food item
-       String servingSize; //Describes the serving size of the food (e.g.,"100g","1 cup")
-       Integer caloriesPerServing; // Number of calories per serving (e.g. 100, 1000)
-       //If we got extra time more attributes like price, category will be added 
-       
-    public FoodItem() {
-        //No argument constructor
-    }
-    public FoodItem(Long id, String name, String servingSize, Integer caloriesPerServing) {
-        // Constructor with all the attributes
-        this.id = id;
-        this.name = name;
-        this.servingSize = servingSize;
-        this.caloriesPerServing = caloriesPerServing;
-    }
+    package com.cbfacademy.apiassessment.model;
     
+    import javax.persistence.Entity;
+    import javax.persistence.GeneratedValue;
+    import javax.persistence.GenerationType;
+    import javax.persistence.Id;
+
+    import org.aspectj.weaver.ast.And;
+    
+    @Entity
+    public class FoodItem {
+    
+        @Id 
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        private Long id;  //Unique identifier for each food item (check if private is needed?) 
+    
+        private String name;  //Name of the food item
+    
+        private String servingSize; //Describes the serving size of the food (e.g.,"100g","1 cup")
+    
+        private Integer caloriesPerServing;   // Number of calories per serving (e.g. 100, 1000)
+           //If we got extra time more attributes like price, category will be added 
+           
+        public FoodItem() {
+            //No argument constructor
+        }
+           // Constructor with all the attributes
+        public FoodItem(Long id, String name, String servingSize, Integer caloriesPerServing) {
+            this.id = id;
+            this.name = name;
+            this.servingSize = servingSize;
+            this.caloriesPerServing = caloriesPerServing;
+        }
+    
+        // Getters and setters for all attributes
+    
+        @Override
+        public String toString() {
+            return "FoodItem{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", servingSize='" + servingSize + '\'' +
+                    ", caloriesPerServing=" + caloriesPerServing +
+                    '}';
+        }
+    
+    }
     //Getters and setters for all attributes
 
     @Override
