14a6c4d0ff044a2d5057ef9986fe5db967566345
Mariejlo <mjlricorico@gmail.com>
2024-01-18 01:10:49 +0100

    Creation of attributes aka instance variables for foodItem class

--- src/main/java/com/cbfacademy/apiassessment/model/fooditem.java
+++ src/main/java/com/cbfacademy/apiassessment/model/fooditem.java
@@ -1,5 +1,11 @@
 package com.cbfacademy.apiassessment.model;
 
-public class fooditem {
-    
-}
+public class foodItem {
+    public static void main(String[] args) {
+       Long id; //Unique identifier for each food item (check if private is needed?) 
+       String name; //Name of the food item
+       String servingSize; //Describes the serving size of the food (e.g.,"100g","1 cup")
+       Integer caloriesPerServing; // Number of calories per serving (e.g. 100, 1000)
+       //If we got extra time more attributes like price, category will be added  
+    }
+}  
