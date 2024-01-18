a403b7b8e3b075bfe02137f71c8211eb9ac765df
Mariejlo <mjlricorico@gmail.com>
2024-01-18 06:16:06 +0100

    Implementation global exception handling by creating the class GlobalExceptionHandler Annotated the class with @ControllerAdvice Defined exception handling methods with @Exception Handler Defined another method for generic exceptions handling

---
+++ src/main/java/com/cbfacademy/apiassessment/advice/globalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.cbfacademy.apiassessment.advice;
+
+public class globalExceptionHandler {
+    
+}
+package com.foodcatalogue.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+
+@ControllerAdvice //Methods are now applicable to all controllers
+public class GlobalExceptionHandler {
+
+    // Exception handlers go here
+
+    @ExceptionHandler(ResourceNotFoundException.class) //Method should be invoked when the exception is thrown
+    @ResponseStatus(HttpStatus.NOT_FOUND) //Status code should be 404
+    @ResponseBody //Return type should be JSON
+    public ErrorMessage handleResourceNotFoundException(ResourceNotFoundException ex) { 
+        return new ErrorMessage(HttpStatus.NOT_FOUND.value(), ex.getMessage());
+
+    @ExceptionHandler(Exception.class)  
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) //Status code should be 500 
+    @ResponseBody //Return type should be JSON 
+    public ErrorMessage handleException(Exception ex) { 
+        return new ErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR.value(), "An  unexpected error has occurred");
+        
+    
+}
