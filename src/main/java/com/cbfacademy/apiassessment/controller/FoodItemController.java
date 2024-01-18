a9ed0fe4968cc9f2044d8422b46643ed12b60f8f
Mariejlo <mjlricorico@gmail.com>
2024-01-18 07:05:33 +0100

    Cleaned and organised FoodItemController package

--- src/main/java/com/cbfacademy/apiassessment/controller/FoodItemController.java
+++ src/main/java/com/cbfacademy/apiassessment/controller/FoodItemController.java
@@ -1,4 +1,5 @@
-package com.cbfacademy.apiassessment.controller;
+
+package com.foodcatalogue.controller; 
 
 public class FoodItemController {
 
@@ -8,7 +9,6 @@ public class FoodItemController {
 //Needs Attributes, Methods, and Constructor (check PascalCase name convention)
 //Optional classes NutritionInfo.java and FoodCategory.java
 //Remember each class single responsibility!
-package com.foodcatalogue.controller; 
 
 import com.foodcatalogue.model.FoodItem;
 
@@ -30,12 +30,12 @@ import java.util.List;
 @RequestMapping("/api/v1/foodItems") //Set the base path for all methods
 @Validated //FoodItem are automatically validated when they are received as input
 
-{
+
     @Autowired
     private FoodItemService foodItemService;
 }
-    @GetMapping("/{id}") //Fetches all the food items from the database 
-    public List<FoodItem> getAllFoodItems() { 
+    @GetMapping List<FoodItem> getAllFoodItems() { //Fetches all the food items from the database (REVISION NEEDED)
+     
         return foodItemService.getAllFoodItems();
     }
     @GetMapping("/{id}") //Fetches a specific food item/single responsibility by ID
@@ -43,15 +43,10 @@ import java.util.List;
         FoodItem foodItem = foodItemService.getFoodItemById(id); 
         return ResponseEntity.ok().body(foodItem);
     }
-    @PostMapping //Creates a new food item
-    public ResponseEntity<FoodItem> createFoodItem(@RequestBody FoodItem foodItem) {  //Method parameter bound to body of the web request
-        FoodItem savedFoodItem = foodItemService.saveFoodItem(foodItem); 
-        return ResponseEntity.ok().body(savedFoodItem);
-    }
-
-    @PostMapping
-    public ResponseEntity<FoodItem> createFoodItem(@Valid @RequestBody FoodItem foodItem) {
-        FoodItem savedFoodItem = foodItemService.saveFoodItem(foodItem);
+   
+    @PostMapping // Createa a new food item with validation
+    public ResponseEntity<FoodItem> createFoodItem(@Valid @RequestBody FoodItem foodItem) { //@Valid if validation fails , Spring return 400 bad request response
+        FoodItem savedFoodItem = foodItemService.saveFoodItem(foodItem); //Method parameter bound to body of the web request
         return ResponseEntity.ok().body(savedFoodItem);
     }
     @PutMapping("/{id}") //Updates a specific food item with new details
