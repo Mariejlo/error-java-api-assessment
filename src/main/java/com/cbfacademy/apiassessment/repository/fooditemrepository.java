b3d7afb9e15122f1e3ab74990bb41bbc28675c78
Mariejlo <mjlricorico@gmail.com>
2024-01-18 02:53:37 +0100

    Interface created in FoodItemRepository with basic CRUD operations Chosen JPA repository instead CRUDrepository= provides more functionalities Some messages added for future references

--- src/main/java/com/cbfacademy/apiassessment/repository/fooditemrepository.java
+++ src/main/java/com/cbfacademy/apiassessment/repository/fooditemrepository.java
@@ -1,7 +1,19 @@
 package com.cbfacademy.apiassessment.repository;
 
-public class fooditemrepository {
+public class foodItemRepository {
     
 }
-//Interfaces adding stock, check whats is available, updating quanitties, removing iterms
+//Interfaces here adding stock, check whats is available, updating quantities, removing items
 //Interacts with database JSOn file to ensure maintenance.
+package com.foodcatalogue.repository;
+
+import com.foodcatalogue.model.FoodItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository //interface is a repository component helpful for scanning and exception translation 
+public interface FoodItemRepository extends JpaRepository<FoodItem, Long> {
+    // Define custom query methods here (optional)
+    //E.g food items with specific name.. method isList<FoodItem> findByName(String name);
+
+}
