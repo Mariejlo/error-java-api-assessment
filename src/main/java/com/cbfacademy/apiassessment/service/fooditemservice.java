cf2230625bfad7009303893e58fb62ccc4eecda3
Mariejlo <mjlricorico@gmail.com>
2024-01-18 03:23:51 +0100

    Interface defined makes code modular and testable Created a class that implement the interface FoodItemService using CRUD operations, it contains business logic Annotations used @Service, @Autowired and @Override

--- src/main/java/com/cbfacademy/apiassessment/service/fooditemservice.java
+++ src/main/java/com/cbfacademy/apiassessment/service/fooditemservice.java
@@ -3,3 +3,79 @@ package com.cbfacademy.apiassessment.service;
 public class fooditemservice {
     
 }
+package com.foodcatalogue.service;
+
+import com.foodcatalogue.model.FoodItem;
+
+import java.util.List;
+
+public interface FoodItemService {
+    
+    FoodItem saveFoodItem(FoodItem foodItem);
+    
+    List<FoodItem> getAllFoodItems();
+    
+    FoodItem getFoodItemById(Long id);
+    
+    FoodItem updateFoodItem(Long id, FoodItem foodItemDetails);
+    
+    void deleteFoodItem(Long id);
+    
+    // Additional methods like calculateTotalCalories can be defined here
+}
+//Implement the interface
+
+package com.foodcatalogue.service;
+
+import com.foodcatalogue.model.FoodItem;
+import com.foodcatalogue.repository.FoodItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+
+public class FoodItemServiceImpl implements FoodItemService {
+
+    @Autowired
+    private FoodItemRepository foodItemRepository;
+
+    @Override
+    public FoodItem saveFoodItem(FoodItem foodItem) {
+        //Logic to save a food item
+        return foodItemRepository.save(foodItem);
+    }
+
+    @Override
+    public List<FoodItem> getAllFoodItems() {
+        //Logic to get all food items
+        return foodItemRepository.findAll();
+    }
+
+    @Override
+    public FoodItem getFoodItemById(Long id) {
+        //Logic to get a food item by id
+        return foodItemRepository.findOne(id).orElseThrow() -> new ResourceNotFoundException("FoodItem", "id", id));
+    }
+    
+    @Override
+    public FoodItem updateFoodItem(Long id, FoodItem foodItemDetails) {  
+        //Logic to update a food item
+        return foodItemRepository.save(foodItemDetails);
+    
+    }
+
+    @Override
+    public void deleteFoodItem(Long id) {
+        //Logic to delete a food item
+        foodItemRepository.delete(id);
+    
+    }
+
+    //Implement additional methods like calculateTotalCalories here
+
+}
+
+    
+    
