cee2b06a42649ef4164d9f774b70ff2ecb50137c
Mariejlo <mjlricorico@gmail.com>
2024-01-20 17:51:29 +0100

    Moved App.java from service package into root package com

--- src/main/java/com/cbfacademy/App.java
+++ src/main/java/com/cbfacademy/App.java
@@ -1,4 +1,4 @@
-package foodcatalogue.subclassfooditem;
+package com.cbfacademy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
