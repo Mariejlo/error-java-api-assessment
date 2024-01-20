cb5254c385c971efe8eb8674706e84fbf06e80ee
Mariejlo <mjlricorico@gmail.com>
2024-01-20 22:15:08 +0100

    Note added to file

--- src/main/java/com/cbfacademy/foodcatalogue/subclassfooditem/advice/ErrorMessage.java
+++ src/main/java/com/cbfacademy/foodcatalogue/subclassfooditem/advice/ErrorMessage.java
@@ -1,6 +1,6 @@
 package com.cbfacademy.foodcatalogue.subclassfooditem.advice;
 
-public class ErrorMessage {
+public class ErrorMessage { //Class to send back consistent error response format
     
 
     private final int statusCode;
