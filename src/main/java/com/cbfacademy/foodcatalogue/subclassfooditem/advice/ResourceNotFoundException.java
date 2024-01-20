8be6a91aceddbc9ccd5b950f503c7bd35663624f
Mariejlo <mjlricorico@gmail.com>
2024-01-20 22:18:40 +0100

    New class created to reduce errors

---
+++ src/main/java/com/cbfacademy/foodcatalogue/subclassfooditem/advice/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.cbfacademy.foodcatalogue.subclassfooditem.advice;
+
+public class ResourceNotFoundException {
+
+    public String getMessage() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getMessage'");
+    }
+
+}
