e83ab9182dc549403de2b370cff7e88d8d063265
Mariejlo <mjlricorico@gmail.com>
2024-01-22 16:37:49 +0100

    Added "MethodArgumentNotValidException"

--- src/main/java/com/cbfacademy/foodcatalogue/subclassfooditem/advice/GlobalExceptionHandler.java
+++ src/main/java/com/cbfacademy/foodcatalogue/subclassfooditem/advice/GlobalExceptionHandler.java
@@ -1,10 +1,18 @@
 package com.foodcatalogue.exception; 
 
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.stream.Collectors;
+
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 import com.cbfacademy.foodcatalogue.subclassfooditem.exception.ResourceNotFoundException; // Correct import
 
 
@@ -25,6 +33,17 @@ public class GlobalExceptionHandler {
     @ResponseBody //Return type should be JSON 
     public ErrorMessage handleException(Exception ex) { 
         return new ErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR.value(), "An  unexpected error has occurred");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+        ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), errors); // custom class to structure error messages
+        return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
     }
 }
 
