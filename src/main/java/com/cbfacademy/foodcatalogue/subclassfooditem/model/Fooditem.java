481b5ccfe4beb9e0d824f02cd972bad8d71d0581
Mariejlo <mjlricorico@gmail.com>
2024-02-17 13:17:23 +0100

    Fixed some attributes that weren't neeeded, structural and formating errors too. Fixed getters and setters section

--- src/main/java/com/cbfacademy/foodcatalogue/subclassfooditem/model/Fooditem.java
+++ src/main/java/com/cbfacademy/foodcatalogue/subclassfooditem/model/Fooditem.java
@@ -1,57 +1,51 @@
-    package com.cbfacademy.foodcatalogue.subclassfooditem.model;
-    
-    import javax.persistence.Entity;
-    import javax.persistence.GeneratedValue;
-    import javax.persistence.GenerationType;
-    import javax.persistence.Id;
-    import javax.persistence.Column;
-    import javax.validation.constraints.Min;
-    import javax.validation.constraints.NotNull;
-    import javax.validation.constraints.Size;
- /**
-* Represents a specific food item in the catalogue.
-* Includes nutritional information and a method for calculating total calories.
-*/
+package com.cbfacademy.foodcatalogue.subclassfooditem.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Column;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class FoodItem extends FoodCatalogue {
    // FoodItem specific attributes, constructors, methods
 
-       
     @Entity // Specifies that this class is an entity and is mapped to a database table
     public class FoodItem {
-        
-        @Id 
+
+        @Id
         @GeneratedValue(strategy = GenerationType.AUTO)
-        private Long id;  //Unique identifier for each food item (check if private is needed?) 
+        private Long id; // Unique identifier for each food item (check if private is needed?)
 
+        @NotNull(message = "Name cannot be null") // Ensures field not null
+        @Size(min = 2, max = 100, message = "Name must be between 2 and 100 characters") // Filed size within range
+        private String name; // Name of the food item
 
-        @NotNull(message = "Name cannot be null") //Ensures field not null
-        @Size(min = 2, max = 100, message = "Name must be between 2 and 100 characters") // Filed size within range        private String name;
-        @Column(name = "food_name", nullable = false, length = 100)
-        private String name;  //Name of the food item
-    
         @NotNull(message = "Serving size cannot be null")
-        private String servingSize; //Describes the serving size of the food (e.g.,"100g","1 cup")
+        private String servingSize; // Describes the serving size of the food (e.g.,"100g","1 cup")
 
         @NotNull(message = "Calories per serving cannot be null")
-        private Integer caloriesPerServing;   // Number of calories per serving (e.g. 100, 1000)
-           //If we got extra time more attributes like price, category will be added 
-         
+        private Integer caloriesPerServing; // Number of calories per serving (e.g. 100, 1000)
+        // If we got extra time more attributes like price, category will be added
+
         // Nutritional information
-        @Min(value = 0, message = "Calories cannot be negative") 
+        @Min(value = 0, message = "Calories cannot be negative")
         private double calories;
 
-        @Min(value = 0, message = "Fats cannot be negative") 
+        @Min(value = 0, message = "Fats cannot be negative")
         private double fats;
 
-        @Min(value = 0, message = "Proteins cannot be negative") 
+        @Min(value = 0, message = "Proteins cannot be negative")
         private double proteins;
 
-        @Min(value = 0, message = "Carbohydrates cannot be negative") 
+        @Min(value = 0, message = "Carbohydrates cannot be negative")
         private double carbohydrates;
 
-        @Min(value = 0, message = "Sugars cannot be negative") 
+        @Min(value = 0, message = "Sugars cannot be negative")
         private double sugars;
-        
+
         public FoodItem() {
             // Constructor code no arguments
         }
@@ -59,25 +53,27 @@ public class FoodItem extends FoodCatalogue {
         private Long id;
         private String name;
         private String servingSize;
-        private Integer caloriesPerServing;    
-   
-        }
-        public FoodItem() {
-            //No argument constructor
-        }
-           // Constructor with all the attributes
-        public FoodItem(Long id, String name, String servingSize, Integer caloriesPerServing) {
-            this.id = id;
-            this.name = name;
-            this.servingSize = servingSize;
-            this.caloriesPerServing = caloriesPerServing;
-            this.fats = fats;
-            this.proteins = proteins;
-            this.carbohydrates = carbohydrates;
-            this.sugars = sugars;
-        }
-    
-    //Getters and setters for all attributes
+        private Integer caloriesPerServing;
+
+    }
+
+    public FoodItem() {
+        // No argument constructor
+    }
+
+    // Constructor with all the attributes
+    public FoodItem(Long id, String name, String servingSize, Integer caloriesPerServing) {
+        this.id = id;
+        this.name = name;
+        this.servingSize = servingSize;
+        this.caloriesPerServing = caloriesPerServing;
+        this.fats = fats;
+        this.proteins = proteins;
+        this.carbohydrates = carbohydrates;
+        this.sugars = sugars;
+    }
+
+    // Getters and setters for all attributes
     public Long getId() {
         return id;
     }
@@ -149,20 +145,26 @@ public class FoodItem extends FoodCatalogue {
     public void setSugars(double sugars) {
         this.sugars = sugars;
     }
+
+    public double calculateTotalCalories() {
+        return fats * 9 + proteins * 4 + carbohydrates * 4;
+    }
+
     @Override
     public String toString() {
-        String servingSize;
-        String caloriesPerServing;
-        String id;
-        String name;
         return "FoodItem{" +
-               "id=" + id +
-               ", name='" + name + '\'' +
-               ", servingSize='" + servingSize + '\'' +
-               ", caloriesPerServing=" + caloriesPerServing +
-               '}';
-
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", servingSize='" + servingSize + '\'' +
+                ", caloriesPerServing=" + caloriesPerServing +
+                ", calories=" + calories +
+                ", fats=" + fats +
+                ", proteins=" + proteins +
+                ", carbohydrates=" + carbohydrates +
+                ", sugars=" + sugars +
+                '}';
     }
+}
       /**
        * Calculates the total caloric value of the food item based on its macronutrient content.
        * Uses the standard nutritional values:
