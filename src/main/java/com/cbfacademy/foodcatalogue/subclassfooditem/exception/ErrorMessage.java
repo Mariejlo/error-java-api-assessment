6a3e7dc1907601b19ea0f5c7c55a930bf28fed37
Mariejlo <mjlricorico@gmail.com>
2024-02-18 20:07:18 +0100

    a¨Handling exception method added to the ErrorMessage file

--- src/main/java/com/cbfacademy/foodcatalogue/subclassfooditem/exception/ErrorMessage.java
+++ src/main/java/com/cbfacademy/foodcatalogue/subclassfooditem/exception/ErrorMessage.java
@@ -1,5 +1,7 @@
 package com.cbfacademy.foodcatalogue.subclassfooditem.exception;
 
+import org.springframework.http.HttpStatus;
+
 public class ErrorMessage { //Class to send back consistent error response format
     
 
@@ -18,5 +20,18 @@ public class ErrorMessage { //Class to send back consistent error response forma
 
     public String getMessage() {
         return message;
+    }
+
+    // New method for handling exceptions
+    public static ErrorMessage handleException(Exception e) {
+        int statusCode = HttpStatus.INTERNAL_SERVER_ERROR.value();
+        String message = "An unexpected error occurred.";
+
+        if (e instanceof ResourceNotFoundException) {
+            statusCode = ((ResourceNotFoundException) e).getHttpStatus().value();
+            message = e.getMessage();
+        }
+
+        return new ErrorMessage(statusCode, message);
     }
 }
