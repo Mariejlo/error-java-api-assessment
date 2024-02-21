8aae94ff00b86f290ae6c11bb1cfee29600b79cf
Mariejlo <mjlricorico@gmail.com>
2024-02-21 17:48:40 +0100

    Implemented abstract method and updated packages

--- src/main/java/com/cbfacademy/App.java
+++ src/main/java/com/cbfacademy/App.java
@@ -8,11 +8,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @SpringBootApplication
 @RestController
 @EnableWebMvc
