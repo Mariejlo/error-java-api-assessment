a355d924add4e0916c6377e8eafd20868ca87112
Mariejlo <mjlricorico@gmail.com>
2024-02-21 19:34:32 +0100

    Adjusted file to OpenAPI annotations and structure

--- src/main/java/com/cbfacademy/App.java
+++ src/main/java/com/cbfacademy/App.java
@@ -1,17 +1,23 @@
 package com.cbfacademy;
 
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 @SpringBootApplication
 @RestController
-@EnableWebMvc
-@EnableSwagger2
+@OpenAPIDefinition(info = @Info(title = "API Assessment", version = "0.0.1", description = "Demo project for Spring Boot"), servers = @Server(url = "/", description = "Default Server URL"))
+
 public class App {
 
 	public static void main(String[] args) {
@@ -24,18 +30,18 @@ public class App {
 	}
 
 	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any())
+	public GroupedOpenApi publicApi() {
+		return GropedOpenApi.builder()
+				.group("public-apis")
+				.packagesToScan("com.cbfacademy") // Adjust the package to scan for your APIs)
 				.build();
 	}
+
 }
 
-//Main method to start Spring Boot application
-//Controller: Classes handling HTTP requests (e.g., REST controllers)
-//Model: Classes representing entities or data models (e.g., User, FoodItem).
-//Service: Classes containing business logic.
-//Repository: Classes for database interactions.
-//Config: Classes for configuration settings.
+// Main method to start Spring Boot application
+// Controller: Classes handling HTTP requests (e.g., REST controllers)
+// Model: Classes representing entities or data models (e.g., User, FoodItem).
+// Service: Classes containing business logic.
+// Repository: Classes for database interactions.
+// Config: Classes for configuration settings.
