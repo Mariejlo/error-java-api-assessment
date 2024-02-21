fe98057526070230c832534a74def376d4c05c89
Mariejlo <mjlricorico@gmail.com>
2024-02-21 21:33:58 +0100

    Method defined

--- src/main/java/com/cbfacademy/foodcatalogue/subclassfooditem/exception/ResourceNotFoundException.java
+++ src/main/java/com/cbfacademy/foodcatalogue/subclassfooditem/exception/ResourceNotFoundException.java
@@ -2,23 +2,21 @@ package com.cbfacademy.foodcatalogue.subclassfooditem.exception;
 
 import org.springframework.http.HttpStatus;
 
-public class ResourceNotFoundException extends RuntimeException {  //Unchecked exception
+public class ResourceNotFoundException extends RuntimeException { // Unchecked exception
 
     public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
         super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
     }
-    
+
     // You can add more constructors or methods if needed
     public String getMessage() {
-        // Auto-generated method stub
+
         throw new UnsupportedOperationException("Unimplemented method 'getMessage'");
     }
 
     public HttpStatus getHttpStatus() {
-        // TODO Auto-generated method stub
+
         throw new UnsupportedOperationException("Unimplemented method 'getHttpStatus'");
     }
 
 }
-
-
