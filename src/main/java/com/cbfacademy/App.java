0f39ba027856e757dcee7ec3adcf5f8524d59e55
Mariejlo <mjlricorico@gmail.com>
2024-02-21 19:37:37 +0100

    Deleted folder Swagger 2

--- src/main/java/com/cbfacademy/App.java
+++ src/main/java/com/cbfacademy/App.java
@@ -4,12 +4,9 @@ import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
