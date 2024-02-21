fda128a5e0fe5ce2f420620155274e47c7262374
Mariejlo <mjlricorico@gmail.com>
2024-02-21 22:06:52 +0100

    Removed some items so this folder is only REST endpoint tests

--- src/test/java/com/cbfacademy/apiassessment/FoodItemTest.java
+++ src/test/java/com/cbfacademy/apiassessment/FoodItemTest.java
@@ -16,7 +16,7 @@ import java.net.URL;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(classes = App.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class FoodItemTest {
+public class FoodItemTestRestController {
 
 	@LocalServerPort
 	private int port;
@@ -48,37 +48,6 @@ class FoodItemTest {
 		assertEquals(200, response.getStatusCode().value());
 		assertEquals("Hello John", response.getBody());
 	}
-
-	public class FoodItemTest {
-		/**
-		 * This class tests the functionalities of the FoodItem model,
-		 * especially the calculateTotalCalories method.
-		 */
-		@Test
-		public void testCalculateTotalCalories() {
-			// Arrange
-			FoodItem foodItem = new FoodItem();
-			foodItem.setCarbohydrates(10);
-			foodItem.setProteins(10);
-			foodItem.setFats(10);
-			foodItem.setSugars(10);
-			System.out.println(foodItem);
-			System.out.println(foodItem.calculateTotalCalories());
-
-			// Act
-			double actualCalories = foodItem.calculateTotalCalories();
-			System.out.println(actualCalories);
-
-			// Assert
-			double expectedCalories = (10 * 4) + (10 * 4) + (10 * 9);
-			assertEquals(expectedCalories, foodItem.calculateTotalCalories());
-			// ... add more test cases...
-
-
-
-			
-		}
-
-	}
-
 }
+
+	
